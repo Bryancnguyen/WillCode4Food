7e0d04e857b030e3180ddc2bc5f90b71d4e38e2e
Bryan Nguyen <bryannguyen@Corjee.local>
2017-03-11 12:11:16 -0800

    Pushing start up

---
+++ client/components/app.js
@@ -0,0 +1,10 @@
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actionCreators from '../actions/actionCreator.js';
+import Main from './Main';
+
+
+
+const App = Main;
+
+export default App;
